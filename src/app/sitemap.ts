8a02588f6af94da8093fc2cffce89570cebcc0b4
Yusup Juniadi <yusup.juniadi@outlook.com>
2026-07-22 17:16:52 +0700

    redeploy 22072026

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,26 +5,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://webbotpro.com";
 
   return [
-    {
+{
       url: baseUrl,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: `${baseUrl}/services`,
+      url: `${baseUrl}/products`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+     {
+      url: `${baseUrl}/custom`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/portfolio`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/about`,
+      url: `${baseUrl}/about-us`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
